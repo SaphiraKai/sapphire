f91417eec53e4c12758f5878db7fada226c1e83e
Saphira Kai <kai.saphira@gmail.com>
2022-06-12 20:48:33 -0300

    sort install.sh

--- install.sh
+++ install.sh
@@ -7,18 +7,18 @@ IFS='\n'
 PREFIX=${PREFIX:-/usr}
 DESTDIR=${DESTDIR:-}
 
-install -Dvm 755 sapphire.sh  $DESTDIR$PREFIX/bin/sapphire
-install -Dvm 755 main.py      $DESTDIR$PREFIX/share/sapphire/main.py
-install -Dvm 644 functions.py $DESTDIR$PREFIX/share/sapphire/functions.py
-install -Dvm 644 colors.py    $DESTDIR$PREFIX/share/sapphire/colors.py
-install -Dvm 644 voice.py     $DESTDIR$PREFIX/share/sapphire/voice.py
-install -Dvm 644 errors.py    $DESTDIR$PREFIX/share/sapphire/errors.py
-install -Dvm 644 data.py      $DESTDIR$PREFIX/share/sapphire/data.py
-install -Dvm 644 cursor.py    $DESTDIR$PREFIX/share/sapphire/cursor.py
-mkdir -p                      $DESTDIR$PREFIX/share/sapphire/voice/
-install -Dvm 644 voice/*.ogg  $DESTDIR$PREFIX/share/sapphire/voice/
 mkdir -p                      $DESTDIR$PREFIX/share/sapphire/config/
+mkdir -p                      $DESTDIR$PREFIX/share/sapphire/voice/
 install -Dvm 644 config/*.py  $DESTDIR$PREFIX/share/sapphire/config/
+install -Dvm 644 voice/*.ogg  $DESTDIR$PREFIX/share/sapphire/voice/
+install -Dvm 644 colors.py    $DESTDIR$PREFIX/share/sapphire/colors.py
+install -Dvm 644 cursor.py    $DESTDIR$PREFIX/share/sapphire/cursor.py
+install -Dvm 644 data.py      $DESTDIR$PREFIX/share/sapphire/data.py
+install -Dvm 644 errors.py    $DESTDIR$PREFIX/share/sapphire/errors.py
+install -Dvm 644 functions.py $DESTDIR$PREFIX/share/sapphire/functions.py
+install -Dvm 644 voice.py     $DESTDIR$PREFIX/share/sapphire/voice.py
+install -Dvm 755 main.py      $DESTDIR$PREFIX/share/sapphire/main.py
+install -Dvm 755 sapphire.sh  $DESTDIR$PREFIX/bin/sapphire
 
 echo
 echo 'installed successfully'
